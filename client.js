ad41a09cc15e84340ce84d98b00d4a506af43839
Raheem Shah <Raheemshah2809@gmail.com>
2022-12-11 23:07:52 +0000

    updated and cleaned source code

--- client.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.banner').unslider({
-      //  Enable keyboard arrows
-      keys: true,               
-			// Enable dot nav
-      dots: true,
-      delay: 7000
-    });
-});
